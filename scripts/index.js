f7b85ddf2ba9895803823359a8610907c51b8624
Amanda Ramirez <amandaalvarado0129@gmail.com>
2025-11-10 21:40:28 -0600

    Proyecto 8- Etapa 1

---
+++ scripts/index.js
@@ -0,0 +1,31 @@
+const initialCards = [
+  [
+    "name",
+    "Valle de Yosemite",
+    "link",
+    "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_yosemite.jpg",
+    "name",
+    "Lago Luoise",
+    "link",
+    "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lake-louise.jpg",
+    "name",
+    "Montañas Calvas",
+    "link",
+    "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_bald-mountains.jpg",
+    "name",
+    "Latemar",
+    "link",
+    "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_latemar.jpg",
+    "name",
+    "Parque Nacional de la Vanoise",
+    "link",
+    "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_vanoise.jpg",
+    "name",
+    "Lago di Braies",
+    "https://practicum-content.s3.us-west-1.amazonaws.com/web-code/moved_lago.jpg",
+  ],
+];
+
+initialCards.forEach((item) => {
+  console.log(item);
+});
